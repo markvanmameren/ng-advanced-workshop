3bf63d63f83baef57d0105b52b0061b3706ba427
Mark van Mameren <mark.van.mameren@divotion.com>
2024-07-30 10:31:20 +0200

    exercise 2

--- src/app/exercise2/country.service.ts
+++ src/app/exercise2/country.service.ts
@@ -1,20 +1,29 @@
-import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {Country} from './types';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable, map } from "rxjs";
+import { Country, State } from "./types";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class CountryService {
-
   private countries$: Observable<Country[]>;
 
   constructor(private http: HttpClient) {
-    this.countries$ = http.get<Country[]>('http://localhost:3000/countries');
+    this.countries$ = http.get<Country[]>("http://localhost:3000/countries");
   }
 
   getCountries(): Observable<Country[]> {
     return this.countries$;
+  }
+
+  getStates(countryCode: string): Observable<State[]> {
+    return this.http
+      .get<State[]>(`http://localhost:3000/states?countryCode=${countryCode}`)
+      .pipe(
+        map((states) =>
+          states.sort((a, b) => (a.description > b.description ? 1 : -1))
+        )
+      );
   }
 }
--- src/app/exercise2/exercise2.component.ts
+++ src/app/exercise2/exercise2.component.ts
@@ -1,19 +1,22 @@
-import { Component } from '@angular/core';
-import {Observable} from 'rxjs';
-import {Country} from './types';
-import {CountryService} from './country.service';
-import {FormControl} from '@angular/forms';
+import { Component, inject } from "@angular/core";
+import { FormControl } from "@angular/forms";
+import { Observable, switchMap } from "rxjs";
+import { CountryService } from "./country.service";
+import { Country, State } from "./types";
 
 @Component({
-  selector: 'app-exercise2',
-  templateUrl: './exercise2.component.html',
-  styleUrls: ['./exercise2.component.css']
+  selector: "app-exercise2",
+  templateUrl: "./exercise2.component.html",
+  styleUrls: ["./exercise2.component.css"],
 })
 export class Exercise2Component {
+  countryService = inject(CountryService);
 
-  countries$: Observable<Country[]> = this.service.getCountries();
-  countryDropdown = new FormControl<Country['id']>(null);
+  countryDropdown = new FormControl<Country["id"] | null>(null);
+  stateDropdown = new FormControl<State["code"] | null>(null);
 
-  constructor(private service: CountryService) { }
-
+  countries$: Observable<Country[]> = this.countryService.getCountries();
+  states$: Observable<State[]> = this.countryDropdown.valueChanges.pipe(
+    switchMap((countryCode) => this.countryService.getStates(countryCode))
+  );
 }
